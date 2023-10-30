74068a49ec269980df125ad6e9781b0e74952393
Medik <steadyeddie101@hotmail.co.uk>
2023-10-30 10:00:56 +0000

    Final UI changes

--- View/UserControls/ModelDbTabView.xaml.cs
+++ View/UserControls/ModelDbTabView.xaml.cs
@@ -1,19 +1,9 @@
-﻿using System;
+﻿using Microsoft.Win32;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using static ModdingTool.Globals;
-using Microsoft.Win32;
 
 namespace ModdingTool.View.UserControls
 {
